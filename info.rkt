7baa6c80a853ff7b570cc1ff01bfcdccac47a0b6
Asumu Takikawa <asumu@ccs.neu.edu>
2016-06-21 20:12:16 -0400

    Fix version string

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 
-(define version 0.2)
+(define version "0.2")
 (define deps '("base"))
 (define build-deps '("racket-doc" "scribble-lib"))
 (define scribblings '(("ffi-definer-convention.scrbl")))
