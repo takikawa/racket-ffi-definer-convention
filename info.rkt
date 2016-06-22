5b6a361adeb1f079b9fabc80055ce592152a9d9a
Asumu Takikawa <asumu@ccs.neu.edu>
2016-06-21 20:13:52 -0400

    Add collection name

--- info.rkt
+++ info.rkt
@@ -1,6 +1,7 @@
 #lang info
 
 (define version "0.2")
+(define collection "ffi-definer-convention")
 (define deps '("base"))
 (define build-deps '("racket-doc" "scribble-lib"))
 (define scribblings '(("ffi-definer-convention.scrbl")))
